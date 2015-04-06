89232ff635939ab7e6fd3269d2bee1c09f320908
Tyler Nivin <twn346@mail.missouri.edu>
2015-04-05 23:36:01 -0500

    change pawprint to sso_id to be more precise. e.g. myhr uses a 'sso_id'

--- database/master.sql
+++ database/master.sql
@@ -6,14 +6,14 @@ SET search_path TO master;
 
 -- Table: master.user
 -- Columns:
---    pawprint          	- Acts as a user name.
+--    sso_id          	- Acts as a user name.
 --    id 				-  the id of the user.
 --    user-type          - Identifies the role of the user, which is critical in determining their level of access.
 --    name 			- Full name of the user.
 --    regDate  			- The time when an account is first created. Set automatically by a default value.
 CREATE TYPE ROLE AS ENUM ('student', 'instructor', 'admin');
 CREATE TABLE user (
-	pawprint	VARCHAR(6) PRIMARY KEY,
+	sso_id	VARCHAR(6) PRIMARY KEY,
 	id 	INTEGER NOT NULL,
 	user_type ROLE NOT NULL,
 	name 	VARCHAR(30) NOT NULL,
@@ -22,14 +22,14 @@ CREATE TABLE user (
 
 -- Table: master.authentication
 -- Columns:
---    pawprint      - The user name tied to authentication info. References user.
+--    sso_id      - The user name tied to authentication info. References user.
 --    password_hash - The hash of the user's password + salt. Expected to be SHA1.
 --    salt          - The salt to use. Expected to be a SHA1 hash of a random input.
 CREATE TABLE authentication (
-	pawprint 			VARCHAR(6) PRIMARY KEY,
+	sso_id 			VARCHAR(6) PRIMARY KEY,
 	password_hash 	CHAR(40) NOT NULL,
 	salt 			CHAR(40) NOT NULL,
-	FOREIGN KEY (pawprint) REFERENCES user(pawprint)
+	FOREIGN KEY (sso_id) REFERENCES user(sso_id)
 );
 
 -- Table: master.course
@@ -47,20 +47,20 @@ CREATE TABLE course (
 -- Columns:
 --    sectionNum  - The section number of the course.
 --    course_id	- Identifies the course the section is a part of. References course.
---    pawprint 		- The pawprint of the instructor teaching the course. References user.
+--    sso_id 		- The sso_id of the instructor teaching the course. References user.
 CREATE TABLE section (
 	sectionNum 	INTEGER,
 	course_id		INTEGER NOT NULL,
-	pawprint 		VARCHAR(6) NOT NULL,
+	sso_id 		VARCHAR(6) NOT NULL,
 	PRIMARY KEY (sectionNum, course_id),
 	FOREIGN KEY (course_id) REFERENCES course(course_id),
-	FOREIGN KEY (pawprint) REFERENCES user(pawprint)
+	FOREIGN KEY (sso_id) REFERENCES user(sso_id)
 );
 
 -- Table: master.application
 -- Columns:
 --	app_id	- The unique id assigned to each application. Set by a sequence.
---	pawprint	- The pawprint of the student who submitted the application.
+--	sso_id	- The sso_id of the student who submitted the application.
 --	TA_PLA	- Determines if the applicant is applying to be a TA (0) or PLA (1)
 --	name		- Full name of the applicant.
 --	id			- Student id of the applicant.
@@ -85,7 +85,7 @@ CREATE TABLE section (
 CREATE TYPE gradStatus AS ENUM ('MS', 'PHD');
 CREATE TABLE application (
 	app_id	SERIAL PRIMARY KEY,
-	pawprint	VARCHAR(6) NOT NULL,
+	sso_id	VARCHAR(6) NOT NULL,
 	TA_PLA	BOOLEAN NOT NULL,
 	name	VARCHAR(30) NOT NULL,
 	id	INTEGER NOT NULL,
@@ -107,36 +107,36 @@ CREATE TABLE application (
 	eSig	VARCHAR(30) NOT NULL,
 	eDate	VARCHAR(30) NOT NULL,
 	submissionDate	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY (pawprint) REFERENCES user(pawprint)
+	FOREIGN KEY (sso_id) REFERENCES user(sso_id)
 );
 
 -- Table: master.comment
 -- Columns:
 --	comment_id	- A unique ID for each comment. Set by a sequence.
---	pawprint 	- The pawprint of the instructor who made the comment. References user.
+--	sso_id 	- The sso_id of the instructor who made the comment. References user.
 --	app_id	- The ID of the application which the comment is attached to. References application.
 --	date		- The time at which the comment was created. Set automatically by a default value.
 CREATE TABLE comment (
 	comment_id	SERIAL PRIMARY KEY,
-	pawprint	VARCHAR(6) NOT NULL,
+	sso_id	VARCHAR(6) NOT NULL,
 	app_id	SERIAL NOT NULL,
 	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY (pawprint) REFERENCES user(pawprint),
+	FOREIGN KEY (sso_id) REFERENCES user(sso_id),
 	FOREIGN KEY (app_id) REFERENCES application(app_id)
 );
 
 -- Table: master.log
 -- Columns:
 --    log_id     - A unique ID for the log entry. Set by a sequence.
---    pawprint   - The user whose action generated this log entry. References user.
+--    sso_id   - The user whose action generated this log entry. References user.
 --    ip_address - The IP address of the user at the time the log was entered.
 --    log_date   - The date of the log entry. Set automatically by a default value.
 --    action     - What the user did to generate a log entry (i.e., "logged in").
 CREATE TABLE log (
 	log_id  	SERIAL PRIMARY KEY,
-	pawprint 		VARCHAR(30) NOT NULL,
+	sso_id 		VARCHAR(30) NOT NULL,
 	ip_address 	VARCHAR(15) NOT NULL,
 	log_date 	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	action 		VARCHAR(50) NOT NULL,
-	FOREIGN KEY (pawprint) REFERENCES user(pawprint)
+	FOREIGN KEY (sso_id) REFERENCES user(sso_id)
 );
