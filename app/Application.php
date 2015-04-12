cb53124f69ab96ff8e09136b6a4499c50f58c2bc
Tyler Nivin <twn346@mail.missouri.edu>
2015-04-12 14:33:43 -0500

    finish Application.php model. Hard to test right now, will test when we write seed logic.

--- app/Application.php
+++ app/Application.php
@@ -4,6 +4,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model {
 
-	//
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'application';
+
+    // Columns that can NOT be mass-assigned.
+
+    protected $guarded = ['app_id','submitted','date_submitted'];
+
+
+
 
 }
