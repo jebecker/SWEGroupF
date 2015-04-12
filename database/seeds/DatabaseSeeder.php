f80a59115f8ae97bfc6082e735dbca9b927f77f3
Tyler Nivin <twn346@mail.missouri.edu>
2015-04-12 18:40:17 -0500

    Finish seeds for users and course tables.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Course;
 
 class DatabaseSeeder extends Seeder {
 
@@ -16,6 +18,64 @@ class DatabaseSeeder extends Seeder {
 
 		//Example. Leave this here.
 		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+		$this->command->info('User table seeded!');
+
+		$this->call('CourseTableSeeder');
+		$this->command->info('Course table seeded!');
+
 	}
 
+}
+
+class UserTableSeeder extends Seeder {
+
+
+	public function run()
+	{
+		//This line clears the current entries in the User table.
+		DB::table('users')->delete();
+
+		User::create([  'sso_id' => 'twn346',
+			'name'   => 'Tyler Nivin',
+			'email'  => 'twn346@mail.missouri.edu',
+			'user_type' => 'student',
+			'password' => bcrypt('secret')
+		]);
+
+		User::create([  'sso_id' => 'scottg',
+			'name'   => 'Grant Scott',
+			'email'  => 'scottg@missouri.edu',
+			'user_type' => 'instructor',
+			'password' => bcrypt('supersecret')
+		]);
+
+		User::create([  'sso_id' => 'rlc5m8',
+			'name'   => 'Ray',
+			'email'  => 'rlc5m8@mail.missouri.edu',
+			'user_type' => 'admin',
+			'password' => bcrypt('admin')
+		]);
+	}
+}
+
+class CourseTableSeeder extends Seeder {
+
+	public function run()
+	{
+		//This line clears the current entries in the User table.
+		DB::table('course')->delete();
+
+		Course::create([ 	'course_name' => 'cs1050',
+							'ta_pla_limit' => '10'
+		]);
+
+		Course::create([ 	'course_name' => 'cs2050',
+							'ta_pla_limit' => '5'
+		]);
+
+		Course::create([ 	'course_name' => 'cs3050',
+							'ta_pla_limit' => '2'
+		]);
+	}
 }
