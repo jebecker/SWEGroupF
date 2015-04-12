ffa2ef115108220923430bde13ca53dc60a7296b
Tyler Nivin <twn346@mail.missouri.edu>
2015-04-12 18:56:48 -0500

    White space stuff.

--- app/Section.php
+++ app/Section.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class Section extends Model {
+class Section extends Model
+{
 
-	//The database table that this model uses
+    //The database table that this model uses
     protected $table = 'section';
 
     //Turn off increments because our table users a custom primary key.
-    public $incrementing  = false;
+    public $incrementing = false;
 
     //Guard timestamps. All else fillable.
     protected $guarded = ['created_at', 'updated_at'];
-
-
-
 }
