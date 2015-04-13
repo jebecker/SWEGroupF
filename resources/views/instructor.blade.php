cb2f848e775bb496a2042f8258a1c31c9fda7d7d
kllxvd <kllxvd@mail.missouri.edu>
2015-04-13 14:39:48 -0500

    added instructor form view -Kevin

---
+++ resources/views/instructor.blade.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>TA/PLA Application Form</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="startbootstrap-agency-1.0.3/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- link to incorporate css with framework
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+    -->
+
+    <!-- Custom CSS -->
+    <link href="startbootstrap-agency-1.0.3/css/agency.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet'
+          type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">TA/PLA Application</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li>
+                    <a href="/">Login</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+<!-- Header -->
+<header style="height:100vh">
+    <div class="container">
+        <div class="intro-text">
+            <div class="intro-lead-in">Welcome To The TA/PLA Application Form!</div>
+        <a href="#instructor" class="page-scroll btn btn-xl">This will be gone later!</a>
+    </div>
+    </div>
+</header>
+
+<section id = "instructor">
+    <div class="container">
+        <!--mutli level columns-->
+        <div class="col-md-8 col-md-offset-2 text-center">
+            <h2>Instructor Page</h2>
+        </div>
+    </div>
+
+    <div>
+        <br>
+    </div>
+
+     <div class="container" style="max-width:1000px;margin 0 auto;">
+        <form class="form-horizontal">
+             
+        
+
+        <div class = "container col-md-offset-2 col-md-8">
+            <div class = "container col-md-offset-4 col-md">
+                <h4> Select a student</h4>
+            </div>
+                 <div class = "form-group form-group-md">
+                     <select class="form-control input-lg " placeholder = "Applicants">
+                          <option>Joe</option>
+                          <option>Jane</option>
+                          <option>Bob</option>
+                          <option>Rick</option>
+                          <option>Bud</option>
+                    </select>
+                </div>
+        </div>
+        </form>
+         <form class="form-inline">
+                 <div class = "container col-md-offset-2 col-md-8">
+                    <h4> Preference rank? </h4>
+                </div>
+                
+                 <div class = "container col-md-offset-2 col-md-1">
+                 <div class = "form-group form-group-md">
+                     <select class="form-control input-lg " placeholder = "Applicants">
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                    </select>
+                </div>
+            </div>
+
+        <div class = "container col-md-offset-2 col-md-8">
+            <br>
+            <h4> Comment about this student </h4>
+            <textarea class="form-control" rows="3"></textarea>
+            
+            <div class="container text-center" style="width:inherit;">
+                <div class="intro-text">
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </div>
+            </div>
+
+        </div>
+
+       
+        </form>
+    </div>
+</section>
+
+
+
+<!-- jQuery -->
+<script src="startbootstrap-agency-1.0.3/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="startbootstrap-agency-1.0.3/js/bootstrap.min.js"></script>
+
+<!-- script tag to integrate js with framework
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+-->
+
+<!-- Plugin JavaScript -->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="startbootstrap-agency-1.0.3/js/classie.js"></script>
+<script src="startbootstrap-agency-1.0.3/js/cbpAnimatedHeader.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="startbootstrap-agency-1.0.3/js/agency.js"></script>
+
+</body>
