fa8521cea380a03aa7c6f4c908ab1b04906b84e7
Tyler Nivin <tyler.nivin@gmail.com>
2015-04-14 04:02:06 +0000

    Explicitly set PK for Comment model.

--- app/Comment.php
+++ app/Comment.php
@@ -10,4 +10,7 @@ class Comment extends Model {
     // an array of the columns that are not mass assignable.
     protected $guarded = ['comment_id'];
 
+    //Primary key is not the default id so set it.
+    protected $primaryKey = 'comment_id';
+
 }
