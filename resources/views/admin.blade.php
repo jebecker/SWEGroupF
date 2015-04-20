cfcc456ee2144e1a20f2d763de1cbb7a49a437fa
Jayme Becker <beckerjayme@gmail.com>
2015-04-20 13:35:20 -0500

    added all the necessary things into the app.blade.php and gutted out the stuff that was not needed anymore in the instructor, admin, and form page and just extedned app to make it work. I also add some css to give some spacing on the login and register page from the nav bar at the top

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,89 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
+@extends('app')
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>TA/PLA Application Form</title>
-
-    <!-- Bootstrap Core CSS-->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- link to incorporate css with framework
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
-    -->
-
-    <!-- Custom CSS -->
-    <link href="/css/agency.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet'
-          type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- custom css for dataTables -->
-    <script src="//cdn.datatables.net/1.10.6/css/jquery.dataTables.css"></script>
-    <script src="//cdn.datatables.net/plug-ins/1.10.6/integration/bootstrap/3/dataTables.bootstrap.css"></script>
-
-
-</head>
-
-<body id="page-top" class="index">
-
-<!-- Navigation -->
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header page-scroll">
-            <button type="button" class="navbar-toggle" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand page-scroll" href="#page-top">Admin Page</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="hidden">
-                    <a href="#page-top"></a>
-                </li>
-                <li>
-                    <a href="/">Login</a>
-                </li>
-                <li>
-                    <a href="form">Application</a>
-                </li>
-                <li>
-                    <a href="admin">Admin</a>
-                </li>
-                <li>
-                    <a href="instructor">Instructor</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container-fluid -->
-</nav>
+@section('content')
 
 <!-- Admin Section -->
 <section id="admin">
@@ -162,30 +80,4 @@
 
 </section>
 
-
-<!-- jQuery -->
-<script src="/js/jquery.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script> src="/js/bootstrap.min.js"></script>
-
-<!-- script tag to integrate js with framework
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
--->
-
-<!-- Plugin JavaScript -->
-<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-<script src="/js/classie.js"></script>
-<script src="/js/cbpAnimatedHeader.js"></script>
-
-<!-- Custom Theme JavaScript -->
-<script src="/js/agency.js"></script>
-
-<!-- dataTables js library (requires jquery) -->
-<script src="//cdn.datatables.net/1.10.6/js/jquery.dataTables.min.js"></script>
-<script src="//cdn.datatables.net/plug-ins/1.10.6/integration/bootstrap/3/dataTables.bootstrap.js"></script>
-
-
-</body>
-
-</html>
+@stop
