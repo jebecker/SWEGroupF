f14dc90d6a0e79052e8ce780289ef325ec9a2e28
wfbernard <wfbc77@mail.missouri.edu>
2015-04-20 22:05:06 -0500

    Added taTeachesCourse many-many migration'

---
+++ database/migrations/2015_04_21_030138_create_taTeachesCourse_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaTeachesCourseTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+		public function up()
+	{
+		Schema::create('taTeachesCourse', function(Blueprint $table)
+		{
+			$table->string('ta');
+			$table->integer('course_id')->unsigned();
+			$table->timestamps();
+			
+			//constraints
+			$table->primary(['ta_id', 'course_id']);
+			$table->foreign('ta_id')
+					->references('sso_id')->on('users')
+					->onDelete('cascade');
+			$table->foreign('course_id')
+					->references('course_id')->on('course')
+					->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('taTeachesCourse');
+	}
+
+}
