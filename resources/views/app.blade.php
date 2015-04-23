7741b56c07a995bdf425b83a4733f10870b4eb97
Jayme Becker <beckerjayme@gmail.com>
2015-04-23 12:35:22 -0500

    Changed the app.blade.php file to only show the necessary pages on the dropdown menu for a specific user (i.e. admin, instructor, etc). I also took out the Laravel menu item in the navbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -50,26 +50,20 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
+				<a class="navbar-brand" href="/">Home</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="/">Home</a></li>
-				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
 						<li><a href="/auth/login">Login</a></li>
-					@else
+					@elseif(Auth::user()->user_type === "instructor")
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
 								<li>
 									<a href="form">Application</a>
-								</li>
-								<li>
-									<a href="admin">Admin</a>
 								</li>
 								<li>
 									<a href="instructor">Instructor</a>
@@ -79,6 +73,33 @@
 								</li>
 							</ul>
 						</li>
+					@elseif(Auth::user()->user_type === "admin")
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li>
+								<a href="form">Application</a>
+							</li>
+							<li>
+								<a href="admin">Admin</a>
+							</li>
+							<li>
+								<a href="/auth/logout">Logout</a>
+							</li>
+						</ul>
+					</li>
+					@else
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li>
+								<a href="form">Application</a>
+							</li>
+							<li>
+								<a href="/auth/logout">Logout</a>
+							</li>
+						</ul>
+					</li>
 					@endif
 				</ul>
 			</div>
