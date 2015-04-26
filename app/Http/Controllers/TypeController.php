9d66f685e76d92a89b85912295e5d24cbbe542f7
rlc5m8 <rlc5m8@mail.missouri.edu>
2015-04-26 08:14:30 -0500

    Massive commit, form refilling, most of form saving, limited form components shown to those relevant

---
+++ app/Http/Controllers/TypeController.php
@@ -0,0 +1,71 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests\ApplicationRequest;
+use App\Application;
+use Illuminate\Support\Facades\Request;
+
+class FormController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Form Controller
+	|--------------------------------------------------------------------------
+	|
+	| Handles displaying and submitting the form View.
+	|
+	*/
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('student');
+	}
+
+	/**
+	 * Show the application dashboard to the user.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return view('form');
+	}
+	public function select($application_type)
+	{
+		return view('form')->with(array('application_type' => $application_type));
+	}
+	public function submit(ApplicationRequest $request)
+	{
+		$courses = '"' . $request->get('courses_teaching','null') . '"';
+		$desired = '"' . $request->get('courses_and_grades') . '"';
+
+		Application::create([
+
+			'name' => $request->get('name'),
+			'applicant_id' => auth()->user()->sso_id,
+			'application_type' => $request->get('application_type'),
+			'student_id' => $request->get('student_id'),
+			'gpa' => floatval($request->get('gpa')),
+			'pla_info' => $request->get('pla_info', null), // ASK TYLER WHY THIS FIELD IS NECESSARY
+			'degree_track' => $request->get('degree_track', null),
+			'advisor_name' => $request->get('advisor_name'),
+			'phone_number' => $request->get('phone_number'),
+			'email' => $request->get('email'),
+			'graduation_date' => $request->get('graduation_date'),
+			'courses_teaching' => $courses,
+			'courses_taught' => $request->get('courses_taught', null),
+			'courses_and_grades' => $desired,
+			'other_employment' => $request->get('other_employment', null),
+			'speak_score' => floatval($request->get('speak_score', null)),
+			'sem_last_speak' => $request->get('sem_last_speak', null),
+			'gato_status' => $request->get('gato_status'),
+			'onita_status' => $request->get('onita_status', null),
+			'submitted' => true
+		]);
+		return view('form')->withErrors('Submission Successful');
+	}
+}
---
+++ app/Http/Requests/ApplicationRequest.php
@@ -0,0 +1,64 @@
+<?php namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class RequestForm extends Request {
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function messages()
+    {
+        // Additional messages may be required/existing messages may need tweaked, more testing needs to be done.
+        return [
+            'application_type' => 'You must indicate your standing',
+            'name.required' => 'Your full name is required',
+            'student_id.required' => 'Your Student ID is required',
+            'gpa.required' => 'Your GPA is required',
+            'pla_info.required_if' => 'Academic Standing required for undergrad students',
+            'degree_track.required_if' => 'Degree required for graduate students',
+            'advisor_name.required' => 'You must enter your department advisor_name\'s name',
+            'phone_number.required' => 'Your phone_number number is required',
+            'email.required' => 'Your student email is required',
+            'graduation_date.required' => 'You must enter a valid graduation date',
+            'courses_and_grades' => 'You must enter the courses you wish to teach',
+            'speak_score.required_if' => 'International students are required to provide a SPEAK OPT score',
+            'sem_last_speak.required_if' => 'The semester of your last SPEAK test is required',
+            'gato_status.required' => 'You must select a GATO status',
+            'speak_status.required_if' => 'International students must indicate their SPEAK test status',
+            'onita_status.required_without' => 'You must indicate an ONITA status',
+        ];
+
+    }
+
+    public function rules()
+    {
+        $date = date("m-d-Y");
+        return [
+            'application_type' => 'required',
+            'name' => 'required|max:255',
+            'student_id' => 'required|numeric',
+            'gpa' => 'required|min:0|max:4',
+            'pla_info' => 'required_if:application_type,PLA|max:10',
+            'degree_track' => 'required_if:application_type,TA,ITA|max:255',
+            'advisor_name' => 'required_if:application_type,TA,ITA|max:255',
+            'phone_number' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'graduation_date' => "required|date|after:$date",
+            'courses_teaching' => 'max:255',
+            'courses_taught' => 'max:255',
+            'courses_and_grades' => 'max:255',
+            'speak_score' => 'required_if:application_type,ITA|required_with:speak_status|numeric|max:255',
+            'sem_last_speak' => 'required_if:application_type,ITA|alpha_num|min:4|max:255',
+            'other_employment' =>  'max:255',
+            'gato_status' => 'required',
+            'speak_status' => 'required_if:application_type,ITA|required_without:speak_date',
+            'onita_status' => 'required_if:application_type,ITA',
+            'speak_date' => 'date|required_if:application_type,ITA|required_without:speak_status' /// MAY REQUIRE ADDITIONAL RULES
+           ];
+    }
+
+}
+?>
